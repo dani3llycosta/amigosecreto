9369a300e59b4001775bff660625391f0df508b3
Danielly Costa <danyycosta48@gmail.com>
2025-02-10 15:11:38 -0300

    implementar função para atualizar lista de amigos

--- app.js
+++ app.js
@@ -9,11 +9,23 @@ function adicionarAmigo() {
         nomesParticipantes.push(nomeAmigos);
         console.log(nomesParticipantes);
         limparCampo();
+        atualizarListaAmigos();
     }
 }
 
 function limparCampo() {
     let nomes = document.getElementById('amigo');
     nomes.value = ''; 
+}
+
+function atualizarListaAmigos() {
+    let listaDeAmigos = document.getElementById('listaAmigos');
+    listaDeAmigos.innerHTML = "";
+
+    for (let i = 0; i < nomesParticipantes.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = nomesParticipantes[i];
+        listaDeAmigos.appendChild(li);
+    }
 }
 
